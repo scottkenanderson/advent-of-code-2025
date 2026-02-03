ea0d4c863680b117c213c4d2c06ec07f7eca7724
Scott Anderson <scottkenanderson@googlemail.com>
2026-02-03 21:42:30 +0000

    day 9

--- packages/day09/src/index.ts
+++ packages/day09/src/index.ts
@@ -84,75 +84,166 @@ const findGreenSquares = (redSquares: Point[]): Point[] => {
 //   return closest;
 // };
 
-const calculateEdges = (redSquares: Point[]): Point[][] => {
-  const perimeter: Point[] = [];
-  redSquares.forEach((square) => {
-    const nearestY = redSquares.filter((s) => s.x === square.x).toSorted((a, b) => Math.abs(a.y - square.y) - Math.abs(b.y - square.y));
-    console.log(`Nearest Y for ${square}: ${nearestY}`);
-    if (nearestY.length === 0) {
-      return;
-    }
-    const nearestX = redSquares.filter((s) => s.y === nearestY[0].y && s.x > nearestY[0].x).toSorted((b, a) => Math.abs(a.x - nearestY[0].x) - Math.abs(b.x - nearestY[0].x));
-    console.log(`Nearest X for ${square}: ${nearestX}`);
-    Point.pointsBetween(square, nearestX[0]).filter(p=> !perimeter.find(e => e.x === p.x && e.y === p.y)).forEach((p) => perimeter.push(p));
+const findNearestNeighbour = (redSquares: Point[], square: Point, isHorizontal: boolean): Point => {
+  return square;
+}
 
-    if (nearestX.length === 0 || nearestY.length === 0) {
-      return;
-    }
-    Point.pointsBetween(square, nearestY[0]).filter(p=> !perimeter.find(e => e.x === p.x && e.y === p.y)).forEach((p) => perimeter.push(p));
-  });
-  const areas: Point[][] = [];
+const calculatePerimeter = (redSquares: Point[]): Set<string> => {
+  const perimeter: Set<string> = new Set();
+  let isHorizontal = true;
   redSquares.forEach((square) => {
-    const nearestY = areas.filter((s) => s.x === square.x && s.y > square.y).toSorted((b, a) => Math.abs(a.y - square.y) - Math.abs(b.y - square.y));
-    console.log(`Nearest Y for ${square}: ${nearestY}`);
-    if (nearestY.length === 0) {
-      return;
+    if (isHorizontal) {
+      const nearestY = redSquares
+        .filter((s) => s.x === square.x)
+        .filter((s) => s.y !== square.y)
+        .toSorted((a, b) => Math.abs(a.y - square.y) - Math.abs(b.y - square.y));
+      Point.pointsBetween(square, nearestY[0]).filter(p=> !perimeter.has(p.toString())).forEach((p) => perimeter.add(p.toString()));
+      // console.log(`Nearest Y for ${square}: ${nearestY.join(';')}`);
+    } else {
+      const nearestX = redSquares
+        .filter((s) => s.y === square.y)
+        .filter((s) => s.x !== square.x)
+        .toSorted((a, b) => Math.abs(a.x - square.x) - Math.abs(b.x - square.x));
+      Point.pointsBetween(square, nearestX[0]).filter(p=> !perimeter.has(p.toString())).forEach((p) => perimeter.add(p.toString()));
+      // console.log(`Nearest X for ${square}: ${nearestX.join(';')}`);
     }
-    const nearestX = areas.filter((s) => s.y === nearestY[0].y && s.x > nearestY[0].x).toSorted((b, a) => Math.abs(a.x - nearestY[0].x) - Math.abs(b.x - nearestY[0].x));
-    console.log(`Nearest X for ${square}: ${nearestX}`);
-    // Point.pointsBetween(square, nearestX).filter(p=> !edges.find(e => e.x === p.x && e.y === p.y)).forEach((p) => edges.push(p));
+    isHorizontal = !isHorizontal;
+    // const nearestX = redSquares.filter((s) => s.y === nearestY[0].y && s.x > nearestY[0].x).toSorted((b, a) => Math.abs(a.x - nearestY[0].x) - Math.abs(b.x - nearestY[0].x));
 
-    if (nearestX.length === 0 || nearestY.length === 0) {
-      return;
-    }
-    // Point.pointsBetween(square, nearestY).filter(p=> !edges.find(e => e.x === p.x && e.y === p.y)).forEach((p) => edges.push(p));
-    const otherEdge = new Point(nearestX[0].x, nearestY[0].y);
-    console.log(`Adding edge between ${square} and ${otherEdge}`);
-    areas.push([square, otherEdge]);
+    // if (nearestX.length === 0 || nearestY.length === 0) {
+    //   return;
+    // }
   });
-  console.log(areas);
-  return areas;
+  return perimeter;
+}
+
+// const calculateAreas = (redSquares: Point[], perimeter: Point[]): Point[][] => {
+//   const areas: Point[][] = [];
+//   redSquares.forEach((square) => {
+//     const nearestY = perimeter.filter((s) => s.x === square.x && s.y > square.y).toSorted((b, a) => Math.abs(a.y - square.y) - Math.abs(b.y - square.y));
+//     console.log(`Nearest Y for ${square}: ${nearestY}`);
+//     if (nearestY.length === 0) {
+//       return;
+//     }
+//     const nearestX = areas.filter((s) => s.y === nearestY[0].y && s.x > nearestY[0].x).toSorted((b, a) => Math.abs(a.x - nearestY[0].x) - Math.abs(b.x - nearestY[0].x));
+//     console.log(`Nearest X for ${square}: ${nearestX}`);
+//     // Point.pointsBetween(square, nearestX).filter(p=> !edges.find(e => e.x === p.x && e.y === p.y)).forEach((p) => edges.push(p));
+
+//     if (nearestX.length === 0 || nearestY.length === 0) {
+//       return;
+//     }
+//     // Point.pointsBetween(square, nearestY).filter(p=> !edges.find(e => e.x === p.x && e.y === p.y)).forEach((p) => edges.push(p));
+//     const otherEdge = new Point(nearestX[0].x, nearestY[0].y);
+//     console.log(`Adding edge between ${square} and ${otherEdge}`);
+//     areas.push([square, otherEdge]);
+//   });
+//   return areas;
+// }
+
+const hasOnlyRedAndGreen = (p1: Point, p2: Point, perimeter: Set<string>): boolean => {
+  if (p1.x == p2.x || p1.y == p2.y) {
+    return false;
+  }
+  // console.log(`Checking between ${p1} and ${p2} to make sure it doesn't bisect ${Array.from(perimeter).join(';')}`);
+  const horizontalPerimeters = /*new Set(*/[
+    ...Point.pointsBetween(new Point(p1.x, p1.y), new Point(p2.x, p1.y)).slice(1, -1),
+    ...Point.pointsBetween(new Point(p2.x, p2.y), new Point(p1.x, p2.y)).slice(1, -1),
+  ]/*.map(p => p.toString()));*/
+  const verticalPerimeters = /*new Set(*/[
+    ...Point.pointsBetween(new Point(p2.x, p1.y), new Point(p2.x, p2.y)).slice(1, -1),
+    ...Point.pointsBetween(new Point(p1.x, p1.y), new Point(p1.x, p2.y)).slice(1, -1),
+  ]/*.map(p => p.toString()));*/
+  if (horizontalPerimeters.length === 0 && verticalPerimeters.length === 0) {
+    return false;
+  }
+  for (let i = 0; i <horizontalPerimeters.length; i++) {
+    const p = horizontalPerimeters[i];
+    const upper = p.x !== p1.x ? 1 : -1;
+    const next = new Point(p.x, p.y + upper)
+    // const prev = new Point(p.x, p.y - upper)
+    if (perimeter.has(p.toString()) && perimeter.has(next.toString())) { //} && perimeter.has(prev.toString())) {
+      // console.log('bisects')
+      return false;
+    }
+  }
+  for (let i = 0; i <verticalPerimeters.length; i++) {
+    const p = verticalPerimeters[i];
+    const upper = p.y === p1.y ? 1 : -1;
+    const next = new Point(p.x + upper, p.y)
+    // const prev = new Point(p.x - upper, p.y)
+    // console.log(p, next, prev)
+    if (perimeter.has(p.toString()) && perimeter.has(next.toString())) {//} && perimeter.has(prev.toString())) {
+      // console.log('bisects')
+      return false;
+    }
+  }
+  const squarePerimeter = [...horizontalPerimeters, ...verticalPerimeters]; /*Array.from(squarePerimeterSet).map(p => {
+    const [x, y] = p.split(',').map(x => parseInt(x, 10));
+    return new Point(x, y);
+  })*/
+  // console.log(squarePerimeter);
+
+  return true;
 };
 
-const hasOnlyRedAndGreen = (p1: Point, p2: Point, edges: Point[]): boolean => {
-  console.log(`Checking between ${p1} and ${p2}`);
+interface Compression {
+  [keyof: string]: number
+}
 
-  return false;
-};
+const compress = (points: number[]): Compression => {
+  return Object.fromEntries(Array.from(new Set(points)).toSorted((a, b) => a - b).map((v, i) => [v, i]));
+}
 
-export const part2 = (redSquares: Point[]): number => {
+const decompress = (points: number[]): Compression => {
+  return Object.fromEntries(Array.from(new Set(points)).toSorted((a, b) => a - b).map((v, i) => [i, v]));
+}
+
+const compressRedSquares = (redSquares: Point[]): { compressedSquares: Point[], compressions: { xCompress: Compression, yCompress: Compression}, decompressions: { xDecompress: Compression, yDecompress: Compression} }  => {
+  const xCompress = compress(redSquares.map(p => p.x));
+  const yCompress = compress(redSquares.map(p => p.y));
+  const xDecompress = decompress(redSquares.map(p => p.x));
+  const yDecompress = decompress(redSquares.map(p => p.y));
+  console.log(xCompress)
+  console.log(yCompress)
+  console.log(xDecompress)
+  console.log(yDecompress)
+  const compressedSquares = redSquares.map(p => new Point(xCompress[p.x.toString()], yCompress[p.y.toString()]))
+  const compressions = {xCompress, yCompress}
+  const decompressions = {xDecompress, yDecompress}
+  return {compressedSquares, compressions, decompressions};
+}
+
+const findMaxAreaPart2 = (redSquares: Point[], perimeter: Set<string>, decompressions: {xDecompress: Compression, yDecompress: Compression}): number => {
   let maxArea = 0;
-  const edges = calculateEdges(redSquares);
+  const { xDecompress, yDecompress } = decompressions;
   redSquares.forEach((square) => {
     redSquares.forEach((otherSquare) => {
-      if (square !== otherSquare && hasOnlyRedAndGreen(square, otherSquare, edges)) {
-        const area = Point.area(square, otherSquare);
-        if (area > maxArea) {
+      if (square !== otherSquare) { //} && (square.x <= otherSquare.x && square.y <= otherSquare.y)) {
+        const area = Point.area(new Point(xDecompress[square.x], yDecompress[square.y]), new Point(xDecompress[otherSquare.x], yDecompress[otherSquare.y]));
+        if (area <= maxArea) {
+          return;
+        }
+        if (hasOnlyRedAndGreen(square, otherSquare, perimeter)) {
+          // console.log(new Point(xDecompress[square.x], yDecompress[square.y]), new Point(xDecompress[otherSquare.x], yDecompress[otherSquare.y]))
+          // console.log("yrs)", area)
           maxArea = area;
         }
       }
     });
   });
-  // return maxArea;
-  console.log('Red squares:', redSquares.toSorted((a, b) => {
-    if (a.y === b.y) {
-      return a.x - b.x;
-    }
-    return a.y - b.y;
-  }));
-  console.log('Edge squares:', edges.flat());
-  const maxX = Math.max(...redSquares.map((p) => p.x));
-  const maxY = Math.max(...redSquares.map((p) => p.y));
+  return maxArea;
+
+}
+
+export const part2 = (redSquares: Point[]): number => {
+  let maxArea = 0;
+  // console.log(redSquares)
+  // console.log(compressRedSquares(redSquares))
+  const { compressedSquares, decompressions } = compressRedSquares(redSquares);
+  const perimeter = calculatePerimeter(compressedSquares);
+  // console.log('Edge squares:', Array.from(perimeter).flat());
+  const maxX = Math.max(...compressedSquares.map((p) => p.x));
+  const maxY = Math.max(...compressedSquares.map((p) => p.y));
   const data = [];
   for (let y = 0; y <= maxY; y += 1) {
     const row = [];
@@ -161,22 +252,27 @@ export const part2 = (redSquares: Point[]): number => {
     }
     data.push(row);
   }
-  const grid = new Grid(data, ' ');
-  redSquares.forEach((square) => {
-    grid.set(square, '#');
-  });
-  edges.forEach(([start, end]) => {
-    for (let y = Math.min(start.y, end.y); y <= Math.max(start.y, end.y); y += 1) {
-      for (let x = Math.min(start.x, end.x); x <= Math.max(start.x, end.x); x += 1) {
-        const p = new Point(x, y);
-        if (grid.retrieve(p) !== '#') {
-          grid.set(p, 'X');
-        }
-      }
-    }
-  });
-  console.log(grid.toString());
-  return -1;
+  // const grid = new Grid(data, ' ');
+  // perimeter.forEach((square) => {
+  //   const [x, y] = square.split(',').map(s => parseInt(s, 10))
+  //   grid.set(new Point(x, y), '~');
+  // });
+  const maxAreaPart2 = findMaxAreaPart2(compressedSquares, perimeter, decompressions);
+  // compressedSquares.forEach((square) => {
+  //   grid.set(square, '#');
+  // });
+  // edges.forEach(([start, end]) => {
+  //   for (let y = Math.min(start.y, end.y); y <= Math.max(start.y, end.y); y += 1) {
+  //     for (let x = Math.min(start.x, end.x); x <= Math.max(start.x, end.x); x += 1) {
+  //       const p = new Point(x, y);
+  //       if (grid.retrieve(p) !== '#') {
+  //         grid.set(p, 'X');
+  //       }
+  //     }
+  //   }
+  // });
+  // console.log(grid.toString());
+  return maxAreaPart2;
 };
 
 if (filename) {
@@ -184,3 +280,4 @@ if (filename) {
   console.log('Part 1:', part1(redSquares));
   console.log('Part 2:', part2(redSquares));
 }
+=
